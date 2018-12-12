4958a02d8ab8a0c5032ca346d43c7748b2e8a8b8
delida <frank_1981@sina.com>
2018-12-12 13:54:59 +0800

    .

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@
 
 project = u'MOACdocs-chn'
 copyright = u'2018, MOAC'
-author = u'MOAC'
+author = u'MOAC Shanghai'
 
 # The short X.Y version
 version = u''
@@ -74,7 +74,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme' #'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
